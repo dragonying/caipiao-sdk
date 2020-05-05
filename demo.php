ad4401f0a06670287cfa745b543af63319dad5b5
fangyingzhong@caibeitv.com <fangyingzhong@caibeitv.com>
2020-05-05 17:50:48 +0800

    test

---
+++ demo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fangying.zhong
+ * Date: 2020-05-05
+ * Time: 17:04
+ */
+require_once __DIR__.'/vendor/autoload.php';
+use \ZfyCmps\ZfyCpTest\Tool;
+
+
+$a = [1,2,3,4];
+Tool::index();
+p($a);
---
+++ tools.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fangying.zhong
+ * Date: 2020-05-05
+ * Time: 17:02
+ */
+function p($d){
+    print_r($d);
+    exit();
+}
